2c5f0ee4ac40510c8220a0b491e7d5de076f1ba0
Ru Yi Shen <ruyishen2012@gmail.com>
2022-05-07 11:24:18 -0300

    feat : auth(routes/controllers/middlewares) implement - status ok

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,29 +1,34 @@
-import bcrypt from 'bcrypt';
-import { v4 as uuid } from 'uuid';
-import db from '../db.js';
+import bcrypt from "bcrypt";
+import { v4 as uuid } from "uuid";
+import db from "../db.js";
 
 export async function signUp(req, res) {
   const user = req.body;
 
-  const passwordHash = bcrypt.hashSync(user.password, 10);
+  try {
+    const hash = await bcrypt.hash(user.password, 10);
+    user.password = hash;
+    await db.collection("users").insertOne(user);
 
-  await db.collection('users').insertOne({ ...user, password: passwordHash })
-
-  res.sendStatus(201);
+    res.sendStatus(201);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
 }
 
 export async function signIn(req, res) {
-  const { email, password } = req.body;
+  const user = res.locals.user;
 
-  const user = await db.collection('users').findOne({ email });
-
-  if (user && bcrypt.compareSync(password, user.password)) {
+  try {
     const token = uuid();
-
-    await db.collection('sessions').insertOne({ token, userId: user._id });
-
-    res.send(token);
-  } else {
-    res.sendStatus(401);
+    await db.collection("tokens").insertOne({
+      token,
+      userId: user._id,
+    });
+    res.json({ token });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
   }
 }
--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import { signUp, signIn } from './controllers/authController.js';
 
+import { validateSignUp, validateSignIn, validateUser } from './middlewares/authMiddleware.js';
+
 const authRouter = Router();
 
-authRouter.post("/sign-up", signUp);
-authRouter.post("/sign-in", signIn);
+authRouter.post("/sign-up", validateSignUp, signUp);
+authRouter.post("/sign-in", validateSignIn, validateUser, signIn);
 
 export default authRouter;
